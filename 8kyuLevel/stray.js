95c04432aef4058f54be0c9e1b9b23e7e4a36349
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-05-13 21:06:05 +0200

    solve challenges

--- 8kyuLevel/stray.js
+++ 8kyuLevel/stray.js
@@ -1,9 +1,5 @@
 function stray(numbers) {
-  numbers.sort((a, b) => {
-    return a - b;
-  });
-  console.log(numbers);
-  return numbers[0] === numbers[1]
-    ? numbers[numbers.length - 1]
-    : numbers[numbers.length - 1];
+ numbers.sort((a,b)=>{ return a-b})
+ console.log(numbers)
+  return numbers[0]===numbers[1]? numbers[numbers.length-1]:numbers[0];
 }
