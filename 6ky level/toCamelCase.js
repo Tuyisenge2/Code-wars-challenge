569615d3bd1b56f3e3217c4d27dc275f271437bb
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-06-18 19:18:34 +0200

    ft  solve challenge

---
+++ 6ky level/toCamelCase.js
@@ -0,0 +1,17 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+// Examples
+// "the-stealth-warrior" gets converted to "theStealthWarrior"
+
+// "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+// "The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+
+function toCamelCase(str) {
+  return str
+    .split(/-|_/)
+    .map((i, ind) => {
+      return ind == 0 ? i : i.slice(0, 1).toUpperCase() + i.slice(1);
+    })
+    .join("");
+}
