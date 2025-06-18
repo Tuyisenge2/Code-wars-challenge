e123ba27d4b5216e54288009135842d21898dc7a
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-06-18 09:02:57 +0200

    ft  solve challenge

---
+++ 6ky level/isValidWalk.js
@@ -0,0 +1,18 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+
+
+
+function isValidWalk(walk) {
+    if (walk.length !== 10) return false;
+    let x=0;
+    let y=0;
+    let a = walk.map(i=>{
+      if(i ==='n')y++
+     else if(i==='s') y--;
+     else if(i==='e') x++;
+     else if(i==='w') x--;
+      return i;
+    })
+  return x===0 && y===0 ? true : false; 
+}
