315efa9accddf946f41253d30ac86625dc5fd520
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-06-22 11:10:26 +0200

    ft-solve challenge

---
+++ 6ky level/findUniq.js
@@ -0,0 +1,12 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+  // do magic
+  return arr.find((v, i, a) => a.lastIndexOf(v) === a.indexOf(v));
+}
